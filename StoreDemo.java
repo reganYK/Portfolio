632e8cccc804aeffe0596ce0be9be50bf33bf305
reganYK <regan.yazdankhahkenari@student.tdsb.on.ca>
2025-06-01 20:13:57 -0400

    Create StoreDemo.java

---
+++ StoreDemo.java
@@ -0,0 +1,6 @@
+public class StoreDemo {
+	public static void main (String []args) {
+		TShirt theBeatlesGraphic = new TShirt("The Beatles Graphic Tee", 29.99, "M", "Cotton");
+		theBeatlesGraphic.displayInfo();
+	}
+}
